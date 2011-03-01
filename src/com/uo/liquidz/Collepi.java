f8bc28bb5807763297b42eb359a88d322820c6a7
liquidz <liquidz.uo@gmail.com>
2011-03-01 18:57:05 +0900

    add AccountManager code

--- src/com/uo/liquidz/Collepi.java
+++ src/com/uo/liquidz/Collepi.java
@@ -1,5 +1,14 @@
 package com.uo.liquidz;
 
+// imports {{{
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
@@ -12,6 +21,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import android.widget.Toast;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+// }}}
 
 public class Collepi extends Activity
 {
@@ -38,9 +54,19 @@ public class Collepi extends Activity
 				} catch (ActivityNotFoundException e){
 					Toast.makeText(Collepi.this, "error", Toast.LENGTH_LONG).show();
 				}
+
 			}
 		});
     }
+
+	@Override
+	public void onResume(){
+		AccountManager am = AccountManager.get(this);
+		Account[] accounts = am.getAccountsByType("com.google");
+		if(accounts.length > 0){
+			am.getAuthToken(accounts[0], "ah", false, new GetAuthTokenCallback(), null); // ah = appengine
+		}
+	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent){
@@ -49,6 +75,78 @@ public class Collepi extends Activity
 			if(resultCode == RESULT_OK){
 				String barcode = intent.getStringExtra("SCAN_RESULT");
 				text.setText(barcode);
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		boolean ret = super.onCreateOptionsMenu(menu);
+
+		menu.add(0, Menu.FIRST, Menu.none, "menu1").setIcon(R.drawable.ic_menu_help);
+		menu.add(0, Menu.FIRST + 1, Menu.none, "menu2").setIcon(R.drawable.ic_menu_add);
+
+		return ret;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		System.out.println(item.getGroupId());
+		System.out.println(item.getItemId());
+
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	private class GetAuthTokenCallback implements AccountManagerCallback<Bundle> {
+		@Override
+		public void run(AccountManagerFuture<Bundle> amf){
+			Bundle bundle = null;
+
+			try {
+				bundle = amf.getResult();
+				Intent intent = (Intent)bundle.get(AccountManager.KEY_INTENT);
+				if(intent != null){
+					// user input required
+					startActivity(intent);
+				} else {
+					loginGoogle(bundle.getString(AccountManager.KEY_AUTHTOKEN));
+				}
+			} catch(OperationCanceledException e){
+				e.printStackTrace();
+			} catch(AuthenticatorException e){
+				e.printStackTrace();
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+
+		private void loginGoogle(String token){
+			DefaultHttpClient http = new DefaultHttpClient();
+			HttpGet get = new HttpGet("https://www.google.com/accounts/TokenAuth?auth=" + token + "&continue=http://www.google.com/calendar/");
+			HttpResponse res = null;
+
+			try {
+				res = http.execute(get);
+			} catch(ClientProtocolException e){
+				e.printStackTrace();
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+
+			if(res.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+				try {
+					String entity = EntityUtils.toString(response.getEntity);
+					if(entity.contains("invalid")){
+						am.invalidateAuthToken("com.google", token);
+					}
+				} catch(IllegalStatusException e){
+					e.printStackTrace();
+				} catch(IOException e){
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("login failure");
 			}
 		}
 	}
